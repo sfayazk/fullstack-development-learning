72bb37e465298c0abe481bab4c6b32f8ae4bfb23
sfayazk <thefayazk@gmail.com>
2026-01-01 20:20:15 +0530

    added section 18

---
+++ Section18 - Advanced javascript and DOM manipulation/Drum Kit Starting Files/index.js
@@ -0,0 +1,23 @@
+// var drumButtons = document.querySelectorAll(".drum");
+// var drumSounds = ["tom-1", "tom-2", "tom-3", "tom-4", "snare", "crash", "kick"];
+//  for (var i = 0; i < drumButtons.length; i++){
+//     drumButtons[i].addEventListener("click", handleClick);
+//     drumButtons[i] = new Audio('sounds/'+drumSounds[i]+'.mp3');
+//  }
+
+//  function handleClick(){
+//     // alert("I got clicked");
+//     // var audio = new Audio('sounds/tom-1.mp3');
+//     drumButtons[i].play();
+// }
+
+var drumButtons = document.querySelectorAll(".drum");
+var drumSounds = ["tom-1", "tom-2", "tom-3", "tom-4", "snare", "crash", "kick"];
+ for (var i = 0; i < drumButtons.length; i++){
+    drumButtons[i].addEventListener("click", handleClick);
+    
+ }
+
+ function handleClick(){
+
+}
