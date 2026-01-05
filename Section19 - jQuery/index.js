d8deb699296446c7beb63276598a62d53169594f
sfayazk <thefayazk@gmail.com>
2026-01-05 20:28:35 +0530

    section 19 updated

--- Section19 - jQuery/index.js
+++ Section19 - jQuery/index.js
@@ -1,1 +1,18 @@
-$("h1").css("color", "red");
+// for(var i=0; i<6; i++){
+//     document.querySelectorAll("button")[i].addEventListener("click", function(){
+//         document.querySelector("h1").style.color="purple";
+//     });
+// }
+
+// $("button").click(function(){
+//     $("h1").css("color", "red")
+// })
+
+// $("h1").on("mouseover",function(){
+//   $("h1").css("color", "aqua")
+// })
+
+// $("h1").before("<button>new</button>");
+// $("h1").after("<button>new</button>");
+    // $("h1").prepend("<button>new</button>")
+    // $("h1").append("<button>new</button>")
